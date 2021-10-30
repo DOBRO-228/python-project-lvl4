e8a7093dd41cf4b2cd0c13cf27815056e47433f3
DOBRO-228 <matematikaigor@gmail.com>
2021-10-30 21:20:19 +0300

    heroku

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-rwggl@^gxj15z8dnr^&q5!w8jojluw#5*2#as^4=$&ph8tl9yh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
