1ab5a22eb02f38b9c0804fb2d927099e25401892
DOBRO-228 <matematikaigor@gmail.com>
2021-12-21 00:12:37 +0300

    refactor

--- statuses/models.py
+++ statuses/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
